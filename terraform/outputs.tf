672e73c02353de9a09af61dd908ed14cdde30e65
Kim Jansson <kumpe82@gmail.com>
2025-03-25 08:56:10 +0100

    fix: condition on amp endpoint output

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,4 +1,4 @@
 output "amp_workspace_endpoint" {
   description = "The endpoint of the Amazon Managed Prometheus workspace."
-  value       = aws_prometheus_workspace.this[0].prometheus_endpoint
+  value       = var.create_amp_workspace ? aws_prometheus_workspace.this[0].prometheus_endpoint : null
 }
