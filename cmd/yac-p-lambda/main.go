7c12f00682f2bd318934704c1824ade13e8d553f
Kim Jansson <31841417+kjansson@users.noreply.github.com>
2025-04-22 17:10:19 +0200

    feat: adding debug logging (#32)

--- cmd/yac-p-lambda/main.go
+++ cmd/yac-p-lambda/main.go
@@ -28,24 +28,30 @@ func HandleRequest() {
 		panic(err)
 	}
 
+	c.Logger.Log("debug", "Starting yac-p lambda function") // Log the start of the function
+
+	c.Logger.Log("debug", "Collecting metrics")
 	// Gather cloudwatch metrics
 	err = c.CollectMetrics()
 	if err != nil {
 		panic(err)
 	}
 
+	c.Logger.Log("debug", "Extracting metrics")
 	// Extract the metrics from the prometheus registry
 	metrics, err := c.ExtractMetrics()
 	if err != nil {
 		panic(err)
 	}
 
+	c.Logger.Log("debug", "Processing metrics")
 	// Process the metrics into timeseries format
 	timeSeries, err := prom.ProcessMetrics(metrics, c.Logger)
 	if err != nil {
 		panic(err)
 	}
 
+	c.Logger.Log("debug", "Persisting metrics")
 	// Persist the metrics to the remote write endpoint
 	err = c.PersistMetrics(timeSeries) // Send the timeseries to the remote write endpoint
 	if err != nil {
