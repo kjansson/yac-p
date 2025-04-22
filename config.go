730474d5e0ea71197f0fe3f1a21ffc63e873d545
Kim Jansson <kumpe82@gmail.com>
2025-04-22 07:51:21 +0200

    fix: better error handling

--- config.go
+++ config.go
@@ -13,7 +13,6 @@ const (
 )
 
 type YaceConfig struct {
-	//PrometheusRemoteWriteURL                          string
 	YaceCloudwatchConcurrencyPerApiLimitEnabled       string
 	YaceCloudwatchConcurrencyListMetricsLimit         string
 	YaceCloudwatchConcurrencyGetMetricDataLimit       string
@@ -24,8 +23,6 @@ type YaceConfig struct {
 }
 
 func (c *YaceConfig) Init() error {
-	// Load environment variables
-	//c.PrometheusRemoteWriteURL = os.Getenv("PROMETHEUS_REMOTE_WRITE_URL")
 	c.YaceCloudwatchConcurrencyPerApiLimitEnabled = os.Getenv("YACE_CLOUDWATCH_CONCURRENCY_PER_API_LIMIT_ENABLED")
 	c.YaceCloudwatchConcurrencyListMetricsLimit = os.Getenv("YACE_CLOUDWATCH_CONCURRENCY_LIST_METRICS_LIMIT")
 	c.YaceCloudwatchConcurrencyGetMetricDataLimit = os.Getenv("YACE_CLOUDWATCH_CONCURRENCY_GET_METRIC_DATA_LIMIT")
@@ -33,29 +30,25 @@ func (c *YaceConfig) Init() error {
 	c.YaceMetricsPerQuery = os.Getenv("YACE_METRICS_PER_QUERY")
 	c.YaceTaggingAPIConcurrency = os.Getenv("YACE_TAG_CONCURRENCY")
 	c.YaceCloudwatchConcurrency = os.Getenv("YACE_CLOUDWATCH_CONCURRENCY")
-	// if c.PrometheusRemoteWriteURL == "" {
-	// 	return fmt.Errorf("PROMETHEUS_REMOTE_WRITE_URL is not set")
-	// }
 	return nil
 }
 
-func (c *YaceConfig) GetYaceOptions(logger Logger) []yace.OptionsFunc {
+func (c *YaceConfig) GetYaceOptions(logger Logger) ([]yace.OptionsFunc, error) {
 	optFuncs := []yace.OptionsFunc{}
 	var cloudwatchPerApiConcurrencyLimit bool
 	var err error
 	if c.YaceCloudwatchConcurrencyPerApiLimitEnabled != "" {
-		//logger.Debug("Using non-default per API concurrency limit", slog.String("per_api_limit", c.YaceCloudwatchConcurrencyPerApiLimitEnabled))
 		logger.Log("debug", "Using non-default per API concurrency limit", slog.String("per_api_limit", c.YaceCloudwatchConcurrencyPerApiLimitEnabled))
 		cloudwatchPerApiConcurrencyLimit, err = strconv.ParseBool(c.YaceCloudwatchConcurrencyPerApiLimitEnabled)
 		if err != nil {
-			panic(err)
+			return nil, err
 		}
 	}
 	if c.YaceMetricsPerQuery != "" {
 		logger.Log("debug", "Using non-default metrics per query", slog.String("metrics_per_query", c.YaceMetricsPerQuery))
 		val, err := strconv.Atoi(c.YaceMetricsPerQuery)
 		if err != nil {
-			panic(err)
+			return nil, err
 		}
 		optFuncs = append(optFuncs, yace.MetricsPerQuery(val))
 	}
@@ -63,7 +56,7 @@ func (c *YaceConfig) GetYaceOptions(logger Logger) []yace.OptionsFunc {
 		logger.Log("debug", "Using non-default tagging API concurrency", slog.String("tagging_api_concurrency", c.YaceTaggingAPIConcurrency))
 		val, err := strconv.Atoi(c.YaceTaggingAPIConcurrency)
 		if err != nil {
-			panic(err)
+			return nil, err
 		}
 		optFuncs = append(optFuncs, yace.TaggingAPIConcurrency(val))
 	}
@@ -73,7 +66,7 @@ func (c *YaceConfig) GetYaceOptions(logger Logger) []yace.OptionsFunc {
 			logger.Log("debug", "Using non-default cloudwatch concurrency", slog.String("cloudwatch_concurrency", c.YaceCloudwatchConcurrency))
 			val, err := strconv.Atoi(c.YaceCloudwatchConcurrency)
 			if err != nil {
-				panic(err)
+				return nil, err
 			}
 			optFuncs = append(optFuncs, yace.CloudWatchAPIConcurrency(val))
 		}
@@ -83,7 +76,7 @@ func (c *YaceConfig) GetYaceOptions(logger Logger) []yace.OptionsFunc {
 			logger.Log("debug", "Using non-default cloudwatch list metrics concurrency", slog.String("cloudwatch_list_metrics_concurrency", c.YaceCloudwatchConcurrencyListMetricsLimit))
 			val, err := strconv.Atoi(c.YaceCloudwatchConcurrencyListMetricsLimit)
 			if err != nil {
-				panic(err)
+				return nil, err
 			}
 			limits.ListMetrics = val
 		}
@@ -91,7 +84,7 @@ func (c *YaceConfig) GetYaceOptions(logger Logger) []yace.OptionsFunc {
 			logger.Log("debug", "Using non-default cloudwatch get metric data concurrency", slog.String("cloudwatch_get_metric_data_concurrency", c.YaceCloudwatchConcurrencyGetMetricDataLimit))
 			val, err := strconv.Atoi(c.YaceCloudwatchConcurrencyGetMetricDataLimit)
 			if err != nil {
-				panic(err)
+				return nil, err
 			}
 			limits.GetMetricData = val
 		}
@@ -99,11 +92,11 @@ func (c *YaceConfig) GetYaceOptions(logger Logger) []yace.OptionsFunc {
 			logger.Log("debug", "Using non-default cloudwatch get metric statistics concurrency", slog.String("cloudwatch_get_metric_statistics_concurrency", c.YaceCloudwatchConcurrencyGetMetricStatisticsLimit))
 			val, err := strconv.Atoi(c.YaceCloudwatchConcurrencyGetMetricStatisticsLimit)
 			if err != nil {
-				panic(err)
+				return nil, err
 			}
 			limits.GetMetricStatistics = val
 		}
 		optFuncs = append(optFuncs, yace.CloudWatchPerAPILimitConcurrency(limits.ListMetrics, limits.GetMetricData, limits.GetMetricStatistics))
 	}
-	return optFuncs
+	return optFuncs, nil
 }
